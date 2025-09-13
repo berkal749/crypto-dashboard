3dc4c6b642e2f1ba9591d9b133160dc936a214e2
berkal749 <berkalachrefpro@gmail.com>
2025-09-13 15:24:30 +0100

    first commit

---
+++ app/components/Currency.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function Currency() {
+  return (
+    <div className='border-2 h-3xl w-sm'>Currency</div>
+  )
+}
---
+++ app/components/navBar.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { Sun } from 'lucide-react';
+
+export default function () {
+    return (
+        <div className=' w-5xl flex justify-between mt-5'>
+
+            <button>GitHub</button>
+
+
+
+
+            <button> <Sun /></button>
+
+        </div>
+    )
+}
