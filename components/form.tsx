1c2538c3bae8be78a66c0ffeaf3be9a803e5ff17
berkal749 <berkalachrefpro@gmail.com>
2025-09-13 17:11:09 +0100

    ok

--- components/form.tsx
+++ components/form.tsx
@@ -6,12 +6,12 @@ import  Radio  from '@/components/ui/radio'
 
 export default function form() {
   return (
-    <form className='bg-neutral-800 h-5 w-9/10 flex h-56 rounded-xl'>
-      <div className='w-3/5 '>
+    <form className='bg-neutral-800 h-5 w-9/10 flex items-center justify-center h-56 rounded-xl'>
+      <div className='w-3/5 flex  flex-col gap-4 '>
         <Input className='h-14  rounded-md '  placeholder="Curenccy one" />
         <div  className=' h-14 bg-neutral-700 flex items-center  rounded-md ' >  </div>
         </div>
-        <div className='flex flex-col h-24 mt-2 justify-between ml-5'>
+        <div className='flex flex-col  h-28  justify-between ml-5'>
         <Radio></Radio>
         <Radio></Radio>
         </div>
